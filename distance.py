1c3b1221d40a49e4b653662468f88a18817f1fd5
Elijah Obra <eobra8897@sdsu.edu>
2025-03-08 18:07:56 -0800

    1st commit to programming assignment 1 (ultrasonic)

--- distance.py
+++ distance.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 class Distance:
     """
     Class to represent a distance measurement with timestamp.
     """
-    def __init__(self, distance):
+    def __init__(self, distance: float, timestamp: str = None):
         """Initialize the Distance object with distance and timestamp."""
-        pass
+        self.distance = distance
+        self.timestamp = datetime.now().strftime("%H:%M:%S")
 
     def __str__(self):
         """Return a formatted string representation of the Distance object."""
-        pass
+        return f"{self.distance} cm at {self.timestamp}"
